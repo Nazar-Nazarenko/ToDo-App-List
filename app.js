e1b807d6529b194d64f4fe94878575012cd4c8ec
Andrii Gerasymchuk <andrii.gerasymchuk@nextiva.com>
2019-11-23 18:02:54 +0200

    added ajax call

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ for(let i = 0; i < myNodeList.length; i++) {
     div.style.display = "none";
   });
 }
+getFromServer();
+
+function getFromServer() {
+  fetch('https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks')
+  .then(response => response.json())
+  .then(responseJson => {
+    for (let i = 0; i < responseJson.length; i++) {
+      let li = document.createElement('li');
+      let inputValue = responseJson[i].title;
+      let t = document.createTextNode(inputValue);
+      li.appendChild(t);
+    document.getElementById("myUl").appendChild(li);
+  }});
+  
+}
 
 function newElement() {
   let li = document.createElement('li');
@@ -43,3 +58,5 @@ list.addEventListener('click', function(ev) {
     ev.target.classList.toggle('checked');
   }
 }, false);
+
+window.onload = getFromServer;
