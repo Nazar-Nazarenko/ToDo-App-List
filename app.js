fb8c0c6b8dbc30a57260a24447c1aa8069a091a3
Nazar-Nazarenko <nazarenkonazar.td@gmail.com>
2019-11-28 11:32:45 +0300

    global changes

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-let myNodeList = document.getElementsByTagName('LI');
-for(let i = 0; i < myNodeList.length; i++) {
+function createCloseButton() {
   let span = document.createElement("span");
   let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  myNodeList[i].appendChild(span);
 
   span.addEventListener('click', function() {
     let div = this.parentElement;
     div.style.display = "none";
   });
+
+  return span;
 }
 
 function newElement() {
   let li = document.createElement('li');
   let inputValue = document.getElementById("myInput").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
-
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  span.addEventListener('click', function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  });
-
+  let text = document.createTextNode(inputValue);
+  li.appendChild(text);
+  li.appendChild(createCloseButton());
   if (inputValue === "") {
     alert("Field can`t be empty!")
   }else{
@@ -36,10 +25,39 @@ function newElement() {
   }
 }
 
-
 let list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if(ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
+
+
 }, false);
+
+
+
+getFromServer();
+
+function getFromServer() {
+  fetch('https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks')
+  .then(response => response.json())
+  .then(responseJson => {
+    for (let i = 0; i < responseJson.length; i++) {
+      let li = document.createElement('li');
+      let inputId = responseJson[i].id;
+      let inputValue = responseJson[i].title;
+      let inputDate = responseJson[i].due_date;
+      let inputStarred = responseJson[i].starred;
+      let id = document.createTextNode(inputId);
+      let value = document.createTextNode(inputValue);
+      let date = document.createTextNode(inputDate);
+      let starred = document.createTextNode(inputStarred);
+      li.appendChild(id);
+      li.appendChild(value);
+      li.appendChild(date);
+      li.appendChild(starred);
+      li.appendChild(createCloseButton());
+      document.getElementById("myUl").appendChild(li);
+    }});
+
+  }
