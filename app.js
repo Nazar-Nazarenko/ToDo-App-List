4f6b51c52826b2973acc7939c9b96bc6d2b6e5fd
Nazar-Nazarenko <nazarenkonazar.td@gmail.com>
2019-12-11 13:29:16 +0300

    ajax

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function createCloseButton() {
   span.addEventListener('click', function() {
     let div = this.parentElement;
     div.style.display = "none";
+    deleteFromServer();
   });
 
   return span;
@@ -25,12 +26,14 @@ function newElement() {
     document.getElementById("myUl").appendChild(li);
   }
   clearButtons();
+  postToServer();
 }
 
 document.getElementById("myInput").addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     newElement();
     clearButtons();
+    postToServer();
   }
 });
 
@@ -72,6 +75,95 @@ function getFromServer() {
       li.appendChild(dateElementFormat);
       li.appendChild(createCloseButton());
       document.getElementById("myUl").appendChild(li);
-    }});
+    }
+  });
+}
 
+function postToServer() {
+  try {
+    const data = postData(
+      'https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks', {
+      "due_date": "2019-11-05T05:35:38.113Z",
+      "title": "foo bar title",
+      "completed": false,
+      "starred": false });
+    console.log(JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
   }
+}
+
+async function postData(url = 'https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer',
+    body: JSON.stringify(data)
+  });
+  return await response.json();
+}
+
+
+
+function updateToServer() {
+  try {
+    const data = putData('https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks/12', {
+      "due_date": "2019-11-05T05:35:38.113Z",
+      "title": "updated title",
+      "completed": false,
+      "starred": false
+    });
+    console.log(JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function putData(url = 'https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks/12', data = {}) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer',
+    body: JSON.stringify(data)
+  });
+  return await response.json();
+}
+
+
+
+
+function deleteFromServer() {
+  try {
+    const data = deleteData('https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks/12');
+    console.log(JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deleteData(url = 'https://5d4908fd2d59e50014f20f15.mockapi.io/api/v1/tasks/12') {
+  const response = await fetch(url, {
+    method: 'DELETE',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer'
+  });
+  return await response.json();
+}
